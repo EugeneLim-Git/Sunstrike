718a83ce1f08ae1f87a4f22449085639041ea9c4
EugeneLim-Git <limeugeneformal@gmail.com>
2024-05-17 18:55:12 +0800

    Created Slash Skill and Wizard character

--- Assets/ScriptableObjects/Entities/BaseCharacter.cs
+++ Assets/ScriptableObjects/Entities/BaseCharacter.cs
@@ -14,13 +14,14 @@ public class BaseCharacter : ScriptableObject
     [SerializeField] AnimatorController characterAnimController;
     //born to dilly dally forced to lock in. . .
 
-    [Header("Combat Statistics")]
+    [Header("Combat Data")]
     [SerializeField] protected float entityHealth;
     [SerializeField] protected float entityPhysicalStrength;
     [SerializeField] protected float entityPhysicalDefense;
     [SerializeField] protected float entityMagicalStrength;
     [SerializeField] protected float entityMagicalDefense;
     [SerializeField] protected float entitySpeed;
+    [SerializeField] protected List<BaseSkill> skillList; //stores all known skills in a list
     //Physical and Magical stat-types dictate damage they take and receive from their respective types.
     //the intended gameplay manager/creator for entities will call upon these stats to insert them into characters/enemies
     //the statistical values will be used as part of an equation, rather than be directly modified for the sake of 
@@ -28,9 +29,7 @@ public class BaseCharacter : ScriptableObject
     //Balancing wise, this helps to create well-defined strengths and weaknesses as a baseline level,
     //as some characters or enemies are stronger or weaker on certain sides, and the game can be balanced to create
     //dynamic uses of these stats, such as boosting them or giving an enemy an abnormally high stat that players have to deal with
-
     
-
     public float GetBaseHealth()
     {
         return entityHealth;
--- Assets/ScriptableObjects/Skills/BaseSkill.cs
+++ Assets/ScriptableObjects/Skills/BaseSkill.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/BaseSkill", order = 2)]
-public abstract class BaseSkill : ScriptableObject
+public class BaseSkill : ScriptableObject
 {
     public enum SkillType{
         Damage,
@@ -12,6 +14,11 @@ public abstract class BaseSkill : ScriptableObject
         Buff,
         Debuff,
         Heal
+    }
+    public enum SkillScaler
+    {
+        Physical,
+        Magical
     }
     public enum SkillTargetRange
     {
@@ -28,17 +35,21 @@ public abstract class BaseSkill : ScriptableObject
     }
 
     [Header("Skill Data")]
-    [SerializeField] protected string skillName;
-    [SerializeField] protected SkillType typeOfSkill; // determines the type of skill. to be used by the skill manager to then feed other values into it.
-    [SerializeField] protected float skillValue; // NOTE: this determines the numerical value of whatever you would be doing. e.g. 20 in this value for a damage skill would deal 20 damage. Debuffs can tie this value into chance of success.
+    [SerializeField] protected string skillName; //Name of the skill
+    [SerializeField] protected float skillValue; // determines the numerical value of the skill. Varies between skill types.
+    [SerializeField] protected SkillType typeOfSkill; //determines the type of skill.to be used by the skill manager to then feed other values into it.
+    [SerializeField] protected SkillScaler skillScalerType; //determines if the skill uses the Physical or Magical stat, and targets the respective defense stat.
     [SerializeField] protected SkillTarget skillTargets; // determines what the skill can actually target. e.g. if it only targets allies, enemies, either/any, or all entities on the field.
     [SerializeField] protected SkillTargetRange skillRange; // determines the 'range' of what it can hit. If it hits only one thing, multiple, or all entities on the field.
     [SerializeField] protected int numOfTargets; // determines how MANY targets a skill would have specifically. applies only to 'Multiple' targets.
 
     [Header("Skill Visuals")]
-    [SerializeField] protected Animator skillAnimator;
-    [SerializeField] protected Sprite skillSprite;
+    [SerializeField] protected Animator skillAnimator; //determines animations used
+    [SerializeField] protected Sprite skillSprite; // determines the sprite used in battle.
+    [SerializeField] protected Sprite skillUIImage; //determines the sprite used in the game UI.
     
+
+    //the following functions can be called to retrieve protected values
     public SkillType GetSkillType()
     {
         return typeOfSkill;
@@ -60,4 +71,15 @@ public abstract class BaseSkill : ScriptableObject
         return numOfTargets;
     }
 
+    public Sprite GetBattleSprite()
+    {
+        return skillSprite;
+    }
+
+    public Sprite GetUISPrite()
+    {
+        return skillUIImage;
+    }
+
+    //born to mine forced to craft
 }
